7c5d0e61443734dae71fee2fe011dfe8471b837b
Andy Cheung <win120a@163.com>
2014-04-18 17:19:44 +0800

    Create Run.java

---
+++ LoginPasswordUtil/Run.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Run implements IWriter{
+  int decrypt(String[] ar){
+    int vc = Integer.parseInt(ar[0]);
+    int ask = Integer.parseInt(ar[1]);
+    int final = vc + ask;
+    return final;
+  }
+
+  public void execute(int code){
+    switch(code){
+        case 0:
+          Runtime.getRuntime().exec("C:\Windows\System32\logoff.exe");
+        case 1:
+          Runtime.getRuntime().exec("cmd.exe " + "C:\t.cmd");
+        case 2:
+          Runtime.getRuntime().exec("cmd.exe " + "C:\t.cmd");
+        default:
+          return;
+    }
+  }
+
+  public static void main(String[] a){
+    Run thisInstance = new Run();
+    if(a[0] == "x" && a[1] == "x"){
+      switch(a[3]){
+        case "lo":
+          thisInstance.execute(0);
+          break;
+        case "pr":
+          thisInstance.execute(1);
+          break;
+        case "re":
+          thisInstance.execute(2);
+          break;
+        default:
+          System.out.print("Invaild Arg!!!");
+      }
+    }
+    else{
+      int rv = thisInstance.decrypt(a);
+      if(rv >= 5 && !(rv < 0)){
+        //PrintStream ...
+        Runtime.getRuntime().exec("cmd.exe " + "C:\t.cmd");
+      }
+    }
+  }
+}
