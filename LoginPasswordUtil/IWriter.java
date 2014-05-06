7a7783c057cf8c57fbca80cccc5b8a8e66b82028
Andy Cheung <win120a@users.noreply.github.com>
2014-05-06 12:07:48 +0800

    Update IWriter.java

--- LoginPasswordUtil/IWriter.java
+++ LoginPasswordUtil/IWriter.java
@@ -1,4 +1,4 @@
-**/
+/**
    Copyright (C) 2011-2014 AC Inc. (Andy Cheung)
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +12,8 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-/**
+**/
 
-https://github.com/win120a/ACClassRoomUtil/blob/master/README.md
 public interface IWriter{
   String baseCmd="net user Administrator ";
   String armv7a="key";
