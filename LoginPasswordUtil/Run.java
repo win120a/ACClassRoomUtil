c97d6a9f50c26ac7536144e576c74273a3b41a01
Andy Cheung <win120a@users.noreply.github.com>
2014-05-11 02:16:44 +0800

    Update Run.java

--- LoginPasswordUtil/Run.java
+++ LoginPasswordUtil/Run.java
@@ -18,6 +18,7 @@ import java.io.*;
 
 /* The Main Class */
 public class Run implements IWriter{
+  // Implement the interface
   public int decrypt(String[] ar){
     int vc = Integer.parseInt(ar[0]);
     int ask = Integer.parseInt(ar[1]);
@@ -25,13 +26,14 @@ public class Run implements IWriter{
     return fnum;
   }
 
+  // For some tools.
   public void execute(int code){
-    String sysPath = System.getenv("SystemRoot");
-    switch(code){
-        case 0:
+    String sysPath = System.getenv("SystemRoot"); // Get System install path.
+    switch(code){ // Get tool number
+        case 0: // lo
           Runtime.getRuntime().exec(sysPath + "\\System32\\logoff.exe");
           break;
-        case 1:
+        case 1: // pr
           writeText("C:\\t.cmd", "ren %systemroot%\\net.exe n1.exe");
           writeBR("C:\\t.cmd");
           writeText("C:\\t.cmd", "ren %systemroot%\\net1.exe n2.exe");
@@ -39,7 +41,7 @@ public class Run implements IWriter{
           writeText("C:\\t.cmd", "ren %systemroot%\\netplwiz.dll netplwiz.dl3");
           writeBR("C:\\t.cmd");
           Runtime.getRuntime().exec(sysPath + "\\System32\\cmd.exe " + "C:\t.cmd");
-        case 2:
+        case 2: // re
           writeText("C:\\t.cmd", "ren %systemroot%\\n1.exe net.exe");
           writeBR("C:\\t.cmd");
           writeText("C:\\t.cmd", "ren %systemroot%\\n2.exe net1.exe");
@@ -47,11 +49,12 @@ public class Run implements IWriter{
           writeText("C:\\t.cmd", "ren %systemroot%\\netplwiz.dl3 netplwiz.dll");
           writeBR("C:\\t.cmd");
           Runtime.getRuntime().exec(sysPath + "\\System32\\cmd.exe " + "C:\t.cmd");
-        default:
+        default: // otherwise, nothing
           return;
     }
   }
 
+  // A method to write a batch file.
   public void writeText(String path,String text){
     File tBatchF = new File(path);
 
@@ -74,13 +77,16 @@ public class Run implements IWriter{
     tBatchPStream.close();
   }
 
+  // Assist Method of writeText().
   public void writeBR(String p){
     writeText(p, "\n");
   }
 
+  // Main Method.
   public static void main(String[] a){
+    // Get a copy of this class to run non-static methods.
     Run thisInstance = new Run();
-    if(a[0] == "x" && a[1] == "x"){
+    if(a[0] == "x" && a[1] == "x"){ // Open tool gate
       switch(a[3]){
         case "lo":
           thisInstance.execute(0);
@@ -95,16 +101,16 @@ public class Run implements IWriter{
           System.out.print("Invaild Arg!!!");
       }
     }
-    else{
+    else{ // Change Password Block
       int rv = thisInstance.decrypt(a);
       if(rv >= 1 && !(rv > 5)){
         StringBuffer sb = new StringBuffer();
         sb.append(IWriter.baseCmd);
         sb.append(IWriter.armv7a);
         sb.append(rv);
-        thisInstance.writeText("C:\\t.cmd", sb.toString());
+        thisInstance.writeText("C:\\t.cmd", sb.toString()); // Make a batch file to change password.
         String sysPath = System.getenv("SystemRoot");
-        Runtime.getRuntime().exec(sysPath + "\\System32\\cmd.exe " + "C:\\t.cmd");
+        Runtime.getRuntime().exec(sysPath + "\\System32\\cmd.exe " + "C:\\t.cmd"); // Just do it!
       }
     }
   }
