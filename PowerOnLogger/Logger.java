7524ea52f5c18a4b5a7d286f2373e04d684377be
Andy Cheung <win120a@users.noreply.github.com>
2014-05-24 15:29:42 +0800

    Update Logger.java

--- PowerOnLogger/Logger.java
+++ PowerOnLogger/Logger.java
@@ -16,6 +16,6 @@
 
 public interface Logger{
   String getSystemInstallPath();
-  String HalfLogPath = "\\System32\\AC-Engine\\PowerOnLogger;
+  String HalfLogPath = "\\System32\\AC-Engine\\PowerOnLogger";
   String LogFileName = "ACPOL_";
 }
