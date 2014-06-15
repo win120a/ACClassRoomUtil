d87b90f1d9ed7b9f833b83a1ba9b603b5a4a6cab
Andy Cheung <win120a@users.noreply.github.com>
2014-06-15 15:53:34 +0800

    Update Main.cs

--- LoginPasswordUtil/C-Sharp-Version/Main.cs
+++ LoginPasswordUtil/C-Sharp-Version/Main.cs
@@ -19,21 +19,50 @@ This is my first draft of this (C# version), it may not pass the build.
 */
 
 using System;
-using System.Diagnostics.Process;
-using System.Environment;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACLoginPasswordUtil;
+using System.Diagnostics;
 
-namespace ACLoginPasswordUtil{
-  class Main{
-    int decrypt(String[] originalArgs){
-      int first = Int32.Parse(originalArgs[0]);
-      int second = Int32.Parse(originalArgs[1]);
-      int result = first + second;
-      return result;
+
+namespace ACLoginPasswordUtil
+{
+    class Run
+    {
+        int decrypt(String[] originalArgs)
+        {
+            int first = Int32.Parse(originalArgs[0]);
+            int second = Int32.Parse(originalArgs[1]);
+            int result = first + second;
+            return result;
+        }
+
+
+        public static void Main(String[] a)
+        {
+            String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
+            Run thisInstance = new Run();
+            Resources resClass = new Resources();
+            int pswInt = thisInstance.decrypt(a);
+            if (pswInt > 1 && !(pswInt > 5)) // The value check.
+            {
+              StringBuilder sBuilder = new StringBuilder();
+              sBuilder.Append(sysPath);
+              sBuilder.Append("\\System32\\");
+              sBuilder.Append(resClass.baseCmd);
+              sBuilder.Append(resClass.netCmd);
+              String cmdText = sBuilder.ToString();
+              sBuilder.Clear();
+              sBuilder.Append(resClass.armv7a);
+              sBuilder.Append(pswInt);
+              String pswText = sBuilder.ToString();
+              sBuilder.Clear();
+              sBuilder.Append(cmdText);
+              sBuilder.Append(pswText);
+              Process.Start(sBuilder.ToString());
+            }
+        }
     }
-
-    static void main(String[] a){
-      String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
-      int pswInt = decrypt(a);
-      //Process.Start();
-  }
 }
