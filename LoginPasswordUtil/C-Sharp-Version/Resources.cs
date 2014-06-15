9e62e9d67dc75df1ed305534e346b5d8f134e201
Andy Cheung <win120a@users.noreply.github.com>
2014-06-15 15:55:00 +0800

    Create Resources.cs

---
+++ LoginPasswordUtil/C-Sharp-Version/Resources.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACLoginPasswordUtil
+{
+    public class Resources
+    {
+        public String baseCmd = "net.exe";
+        public String netCmd = " user Administrator ";
+        public String armv7a = "key";
+        public String tail = ".";
+    }
+}
