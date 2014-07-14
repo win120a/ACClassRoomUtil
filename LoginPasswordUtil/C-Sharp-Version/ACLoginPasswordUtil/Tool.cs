5af7a278c133fb6a9415cfe55d9d3f3b47518814
Andy Cheung <micro_zx@msn.cn>
2014-07-14 12:01:18 +0800

    Add C# Projects

---
+++ LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Tool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ACLoginPasswordUtil
+{
+    class Tool
+    {
+        public static void ShutdownSystem()
+        {
+            String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
+            Process.Start(sysPath + "\\System32\\shutdown.exe", "-s -t 0");
+        }
+
+        public static void RebootSystem()
+        {
+            String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
+            Process.Start(sysPath + "\\System32\\shutdown.exe", "-r -t 0");
+        }
+
+        public static void LogoffFromSystem()
+        {
+            String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
+            Process.Start(sysPath + "\\System32\\logoff.exe");
+        }
+    }
+}
--- ProcessBlockUtil/PBUNetworkEdition/Properties/AssemblyInfo.cs
+++ ProcessBlockUtil/PBUNetworkEdition/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("PBURuleUpdater")]
-[assembly: AssemblyDescription("This is the updater file of Network ACPBU")]
+[assembly: AssemblyTitle("ACProcessBlockUtil")]
+[assembly: AssemblyDescription("This is the main file of ACPBU")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("AC Inc.")]
-[assembly: AssemblyProduct("PBURuleUpdater")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyProduct("ACProcessBlockUtil")]
+[assembly: AssemblyCopyright("Copyright © AC Inc.")]
 [assembly: AssemblyTrademark("ACClassroomUtil")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c7a6613b-b50b-460b-a8d0-5404f75d130c")]
+[assembly: Guid("5df0a898-3582-4f5a-bc81-6378d9c20e25")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
