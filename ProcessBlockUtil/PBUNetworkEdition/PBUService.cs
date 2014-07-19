e878fe6e35131d594a703329c6f0334c21d345e1
Andy Cheung <micro_zx@msn.cn>
2014-07-19 13:55:58 +0800

    Add Regions

--- ProcessBlockUtil/PBUNetworkEdition/PBUService.cs
+++ ProcessBlockUtil/PBUNetworkEdition/PBUService.cs
@@ -1,4 +1,7 @@
-﻿/*
+﻿#region "开源协议"
+/*
+   THIS IS ONLY TO EASY CHANGE, NOT THE PROJECT CODE AT NOW. (I will use in the future.)
+
    Copyright (C) 2011-2014 AC Inc. (Andy Cheung)
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,54 +16,66 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+#endregion
 
+#region "using语句"
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.ServiceProcess;
 using System.Threading;
+#endregion
 
 namespace ACProcessBlockUtil
 {
     class PBUService : ServiceBase
     {
+        #region "全局变量声明"
         static String[] list;
         PBUService cache;
+        #endregion
 
+        #region "构造方法"
         public PBUService()
         {
-          cache = this;
+            cache = this;
         }
+        #endregion
 
+        #region "核心方法"
         public static void kill()
         {
-          while (true)
-          {
-            foreach (String s in PBUService.list)
+            while (true)
             {
-              Process[] thatProcArray = Process.GetProcessesByName(s);
-              if (thatProcArray.Length == 0)
-              {
-                continue;
-              }
-              foreach (Process p in thatProcArray)
-              {
-                p.Kill();
-              }
-              Thread.Sleep(2000);
+                foreach (String s in PBUService.list)
+                {
+                    Process[] thatProcArray = Process.GetProcessesByName(s);
+                    if (thatProcArray.Length == 0)
+                    {
+                        continue;
+                    }
+                    foreach (Process p in thatProcArray)
+                    {
+                        p.Kill();
+                    }
+                    Thread.Sleep(2000);
+                }
             }
-          }
         }
+        #endregion
 
+        #region "入口方法"
         public static void Main(String[] a)
         {
+          #region "局部变量声明"
           StreamReader sr;
-	  PBUService thisInstance = new PBUService();
-	  String userProfile = Environment.GetEnvironmentVariable("UserProfile");
-	  String systemRoot = Environment.GetEnvironmentVariable("SystemRoot");
-			
-	  if(File.Exists(userProfile + "\\ACRules.txt"))
-          {
+	      PBUService thisInstance = new PBUService();
+	      String userProfile = Environment.GetEnvironmentVariable("UserProfile");
+	      String systemRoot = Environment.GetEnvironmentVariable("SystemRoot");
+          #endregion
+
+          #region "设置列表"
+          if (File.Exists(userProfile + "\\ACRules.txt")){
             sr = new StreamReader(userProfile + "\\ACRules.txt");
             String tempLine = sr.ReadToEnd();
             list = tempLine.Split(',');
@@ -68,16 +83,23 @@ namespace ACProcessBlockUtil
           }
           else
           {
-            PBUService.list = new String[] {"iexplore", "360se", "chrome", "firefox", "safari"};
+		    PBUService.list = new String[] {"iexplore", "360se", "chrome", "firefox", "safari"};
           }
-	  ServiceBase.Run(thisInstance);
-        }
+          #endregion
 
+          #region "运行服务程序"
+          ServiceBase.Run(thisInstance);
+          #endregion
+        }
+        #endregion
+
+        #region "服务入口方法"
         protected override void OnStart(String[] a)
         {
-          Thread t = new Thread(new ThreadStart(kill));
-          t.IsBackground = true;
-          t.Start();
+            Thread t = new Thread(new ThreadStart(kill));
+            t.IsBackground = true;
+            t.Start();
         }
+        #endregion
     }
 }
