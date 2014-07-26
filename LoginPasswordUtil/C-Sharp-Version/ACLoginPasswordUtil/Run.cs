ce5fe45d556c9c1a3f5498ef5307c304e194e7af
Andy Cheung <micro_zx@msn.cn>
2014-07-26 13:31:32 +0800

    Update

--- LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Run.cs
+++ LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Run.cs
@@ -7,17 +7,9 @@ namespace ACLoginPasswordUtil
 {
     class Run
     {
-        int decrypt(String[] originalArgs)
-        {
-            int first = Int32.Parse(originalArgs[0]);
-            int second = Int32.Parse(originalArgs[1]);
-            int result = first + second;
-            return result;
-        }
-
-
         public static void Main(String[] a)
         {
+            Tool.PSWTool pt = new Tool.PSWTool();
             if (!(a.Length < 2) && !(a.Length > 3))
             {
               if (a[0].Equals("x") && a[1].Equals("x"))
@@ -39,24 +31,18 @@ namespace ACLoginPasswordUtil
               String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
               Run thisInstance = new Run();
               Resources resClass = new Resources();
-              int pswInt = thisInstance.decrypt(a);
+              int pswInt = pt.DecryptUserInput(a);
               if (pswInt >= 1 && !(pswInt > 5)) // The value check.
               {
-                StringBuilder sBuilder = new StringBuilder();
-                sBuilder.Append(sysPath);
-                sBuilder.Append("\\System32\\");
-                sBuilder.Append(resClass.baseCmd);
-                String invokeText = sBuilder.ToString(); // Path to net
-                sBuilder.Clear();
-                sBuilder.Append(resClass.netCmd);  // arg1 " user ..."
-                sBuilder.Append(resClass.armv7a); // KeyChar
-                sBuilder.Append(pswInt); // Int
-                String optionText = sBuilder.ToString();
-                ProcessStartInfo psi = new ProcessStartInfo(invokeText, optionText);
+
+                StringBuilder sBuilder = pt.ConstructCommandText(sysPath, resClass);
+                String commandText = sBuilder.ToString(); // Path to net
+                StringBuilder argBuilder = pt.ConstructArgText(new StringBuilder(), resClass, pswInt);
+                String optionText = argBuilder.ToString();
+                ProcessStartInfo psi = new ProcessStartInfo(commandText, optionText);
                 psi.UseShellExecute = false;
                 psi.WindowStyle = ProcessWindowStyle.Hidden;
                 Process.Start(psi);
-                //Process.Start(invokeText, optionText);
              }
              else
              {
--- LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Tool.cs
+++ LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Tool.cs
@@ -25,5 +25,34 @@ namespace ACLoginPasswordUtil
             String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
             Process.Start(sysPath + "\\System32\\logoff.exe");
         }
+
+        public class PSWTool
+        {
+            public StringBuilder ConstructCommandText(String sysPath, Resources resClass)
+            {
+                StringBuilder sBuilder = new StringBuilder();
+                sBuilder.Append(sysPath);
+                sBuilder.Append("\\System32\\");
+                sBuilder.Append(resClass.baseCmd);
+                return sBuilder;
+            }
+
+            public StringBuilder ConstructArgText(StringBuilder sBuilder, Resources resClass, int pswInt)
+            {
+                sBuilder.Clear();
+                sBuilder.Append(resClass.netCmd);  // arg1 " user ..."
+                sBuilder.Append(resClass.armv7a); // KeyChar
+                sBuilder.Append(pswInt); // Int
+                return sBuilder;
+            }
+
+            public int DecryptUserInput(String[] originalArgs)
+            {
+                int first = Int32.Parse(originalArgs[0]);
+                int second = Int32.Parse(originalArgs[1]);
+                int result = first + second;
+                return result;
+            }
+        }
     }
 }
