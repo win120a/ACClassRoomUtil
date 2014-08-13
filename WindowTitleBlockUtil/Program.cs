eccb6ef58f6d45b492a204caae245449eaf42505
Andy Cheung <micro_zx@msn.cn>
2014-08-13 19:00:40 +0800

    Update

---
+++ WindowTitleBlockUtil/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace ACWindowTitleBlockUtil
+{
+    class Program : ServiceBase
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase.Run(new Program());
+        }
+
+        public void findTitle()
+        {
+            while (true)
+            {
+                Process[] processes = Process.GetProcesses();
+                foreach (Process p in processes)
+                {
+                    // p.WaitForInputIdle();
+                    if (p.MainWindowTitle.Contains("nba"))
+                    {
+                       p.Kill();
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    Thread.Sleep(2000);
+                }
+            }
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(findTitle));
+            t.IsBackground = true;
+            t.Name = "Find Thread";
+            t.Start();
+            // TODO
+        }
+    }
+}
--- WindowTitleBlockUtil/Properties/AssemblyInfo.cs
+++ WindowTitleBlockUtil/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("ACProcessBlockUtil")]
-[assembly: AssemblyDescription("This is the main file of ACPBU")]
+[assembly: AssemblyTitle("ACWindowTitleBlockUtil")]
+[assembly: AssemblyDescription("This is the main file of ACWTBU")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("AC Inc.")]
 [assembly: AssemblyProduct("ACClassroomUtil")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("b8395e56-9c3e-45ea-921d-2be38049abbc")]
+[assembly: Guid("6838ce89-061c-4c81-8176-454f4dbd9e26")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
