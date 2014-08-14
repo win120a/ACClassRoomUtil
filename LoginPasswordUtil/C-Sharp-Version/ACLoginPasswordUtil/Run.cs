c4f1e1b631e6fb694cabcc9a412ad84b7c0e2d94
Andy Cheung <micro_zx@msn.cn>
2014-08-14 15:48:19 +0800

    Formatting

--- LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Run.cs
+++ LoginPasswordUtil/C-Sharp-Version/ACLoginPasswordUtil/Run.cs
@@ -30,31 +30,31 @@ namespace ACLoginPasswordUtil
             PSWTool pt = new PSWTool();
             if (!(a.Length < 2) && !(a.Length > 3))
             {
-              if (a[0].Equals("x") && a[1].Equals("x"))
-              {
-                  entry.ChooseTool(a[2]);
-                  return;
-              }
-              String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
-              Resources resClass = new Resources();
-              int pswInt = pt.DecryptUserInput(a);
-              if (pswInt >= 1 && !(pswInt > 5)) // The value check.
-              {
-                StringBuilder sBuilder = pt.ConstructCommandText(sysPath, resClass);
-                String commandText = sBuilder.ToString(); // Path to net
-                StringBuilder argBuilder = pt.ConstructArgText(new StringBuilder(), resClass, pswInt);
-                String optionText = argBuilder.ToString();
-                pt.ChangeSystemPassword(sysPath, resClass, pswInt);
-             }
-             else
-             {
-                 Console.WriteLine("INVAILD DAY OF Week Settings (Monday to Friday only for this Edition), If there is some reasons of this, please change it manually.");
-             }
-          }
-          else
-          {
-              Console.WriteLine("INVAILD ARG LENGTH!");
-          }
-       }
+                if (a[0].Equals("x") && a[1].Equals("x"))
+                {
+                    entry.ChooseTool(a[2]);
+                    return;
+                }
+                String sysPath = Environment.GetEnvironmentVariable("SystemRoot");
+                Resources resClass = new Resources();
+                int pswInt = pt.DecryptUserInput(a);
+                if (pswInt >= 1 && !(pswInt > 5)) // The value check.
+                {
+                    StringBuilder sBuilder = pt.ConstructCommandText(sysPath, resClass);
+                    String commandText = sBuilder.ToString(); // Path to net
+                    StringBuilder argBuilder = pt.ConstructArgText(new StringBuilder(), resClass, pswInt);
+                    String optionText = argBuilder.ToString();
+                    pt.ChangeSystemPassword(sysPath, resClass, pswInt);
+                }
+                else
+                {
+                    Console.WriteLine("INVAILD DAY OF Week Settings (Monday to Friday only for this Edition), If there is some reasons of this, please change it manually.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("INVAILD ARG LENGTH!");
+            }
+         }
     }
 }
