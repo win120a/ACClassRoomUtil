2f2b6a401639512f6d9074d859f49d89e8384212
Andy Cheung <micro_zx@msn.cn>
2014-08-14 21:04:54 +0800

    Add Project

---
+++ PowerOnLogger/PowerOnLoggerManagmentTool/Account.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerOnLoggerManagmentTool
+{
+    public partial class Account : Form
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.User;
+        }
+
+        private void Account_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default.User;
+        }
+
+        private void pc_Click(object sender, EventArgs e)
+        {
+            if (op.Text.Equals(Properties.Settings.Default.Pass))
+            {
+                if (np.Text.Equals(npc.Text))
+                {
+                    Properties.Settings.Default.Pass = np.Text;
+                    Properties.Settings.Default.Save();
+                    MessageBox.Show("Change Password Successful.", "OK");
+                    this.Hide();
+                    new Main().Show();
+                }
+                else
+                {
+                    MessageBox.Show("New Password and Confirm not Match!", "Fail");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Old Password Wrong!", "Fail");
+            }
+        }
+
+        private void uc_Click(object sender, EventArgs e)
+        {
+            if (nu.Text.Equals(nuc.Text))
+            {
+                Properties.Settings.Default.User = nu.Text;
+                Properties.Settings.Default.Save();
+                MessageBox.Show("Change Username Successful.", "OK");
+                this.Hide();
+                new Main().Show();
+            }
+            else
+            {
+                MessageBox.Show("New Username and Confirm not Match!", "Fail");
+            }
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Main().Show();
+        }
+    }
+}
---
+++ PowerOnLogger/PowerOnLoggerManagmentTool/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerOnLoggerManagmentTool
+{
+    public partial class Verify : Form
+    {
+        public Verify()
+        {
+            InitializeComponent();
+        }
+
+        private void co_Click(object sender, EventArgs e)
+        {
+#if DEBUG
+            MessageBox.Show(Properties.Settings.Default.User);
+            MessageBox.Show(Properties.Settings.Default.Pass);
+#endif
+            if ((p.Text.Equals(Properties.Settings.Default.Pass)) && (u.Text.Equals(Properties.Settings.Default.User)))
+            {
+                MessageBox.Show("OK", "OK");
+                this.Hide();
+                new Main().Show();
+            }
+            else
+            {
+                MessageBox.Show("User/Password Wrong!", "Fail");
+            }
+        }
+
+        private void cc_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
