e7fc87d80086ae9cadb941a0af475e180e9bb0cf
Andy Cheung <win120a@users.noreply.github.com>
2014-08-29 18:18:56 +0800

    Add license
    I just forgot for it.

--- PowerOnLogger/ACPowerOnLogger/src/net/ac/clsroomutil/pol/Logger.java
+++ PowerOnLogger/ACPowerOnLogger/src/net/ac/clsroomutil/pol/Logger.java
@@ -1,9 +1,19 @@
 /*
- Copyright (C) 2011-2014 AC Inc.  (Andy Cheung)
+ Copyright (C) 2011-2014 AC Inc. (Andy Cheung)
 
- There is not license selected yet.  (Please select Apache License.)
- Please set in project settings.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
+
 package net.ac.clsroomutil.pol;
 
 import java.io.File;
